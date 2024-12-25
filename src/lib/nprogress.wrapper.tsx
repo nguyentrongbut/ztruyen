6f30090fd0cfe2438360017f1a7ecae4f3074612
Bút <Cloly031128@gmail.com>
2024-12-25 23:59:05 +0700

    fix: fix useSearchParams() should be wrapped in a suspense boundary at page '/404'

--- src/lib/nprogress.wrapper.tsx
+++ src/lib/nprogress.wrapper.tsx
@@ -1,17 +1,20 @@
 'use client';
 
 import { AppProgressBar as ProgressBar } from 'next-nprogress-bar';
+import {Suspense} from "react";
 
 const ProgressWrapper = ({ children } : {children: React.ReactNode}) => {
     return (
         <>
             {children}
-            <ProgressBar
-                height="4px"
-                color="#32aaff"
-                options={{ showSpinner: false }}
-                shallowRouting
-            />
+            <Suspense>
+                <ProgressBar
+                    height="2px"
+                    color="#32aaff"
+                    options={{ showSpinner: false }}
+                    shallowRouting
+                />
+            </Suspense>
         </>
     );
 };
