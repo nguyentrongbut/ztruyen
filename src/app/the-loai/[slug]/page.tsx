c2c2fdffd698afb48c671e0b5f665e1f12c2e03a
Bút <Cloly031128@gmail.com>
2024-12-25 13:35:00 +0700

    ui: remove tag tat ca

--- src/app/the-loai/[slug]/page.tsx
+++ src/app/the-loai/[slug]/page.tsx
@@ -37,11 +37,6 @@ const Genre = async ({
             <nav className="wrapper flex gap-3.5 justify-center container mt-6 mb-8">
                 <p className="flex-shrink-0 text-[15px] dark:text-[#ffffffbd] text-[#00000057]">Thể loại</p>
                 <ul className="flex gap-3.5 flex-wrap text-[15px]">
-                    <li>
-                        <Link href={`/the-loai/tat-ca`}
-                              className={`active:bg-[#32aaff] active:text-primary rounded-[5px] px-[10px] py-1.5 ${slug === "tat-ca" && "text-[#32aaff]"}`}>Tất
-                            cả</Link>
-                    </li>
                     {data.map((item, index) => (
                         <li key={index}>
                             <Link href={`/the-loai/${item.slug}`}
