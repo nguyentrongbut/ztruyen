955b012c74a754ca5aad2322f9d2e013a1bf771a
Bút <Cloly031128@gmail.com>
2025-01-14 12:58:57 +0700

    fix eslint

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,6 @@
 import MySwiper from '@/components/common/Swiper';
 // import axios from "axios";
 import NavbarGenre from '@/components/common/NavbarGenre';
-import Header from '@/components/common/Header';
 
 export default async function Home() {
     // const response = await axios.get(`https://otruyenapi.com/v1/api/home`)
