b5fd7c56e489f336d44cb844912bcdf9f727a3d5
Bút <Cloly031128@gmail.com>
2025-01-20 07:33:41 +0700

    test: gg search console

--- src/app/sitemap.ts
+++ src/app/sitemap.ts
@@ -7,44 +7,26 @@ export default async function sitemap() {
         {
             url: baseURL,
             lastModified: new Date(),
-            changeFrequency: 'weekly',
-            priority: 0.5,
-            // images: ['https://example.com/image.jpg'],
         },
         {
             url: `${baseURL}/truyen-tranh/`,
             lastModified: new Date(),
-            changeFrequency: 'weekly',
-            priority: 0.5,
-            // images: ['https://example.com/image.jpg'],
         },
         {
             url: `${baseURL}/doc-truyen/`,
             lastModified: new Date(),
-            changeFrequency: 'weekly',
-            priority: 0.5,
-            // images: ['https://example.com/image.jpg'],
         },
         {
             url: `${baseURL}/the-loai/`,
             lastModified: new Date(),
-            changeFrequency: 'weekly',
-            priority: 0.5,
-            // images: ['https://example.com/image.jpg'],
         },
         {
             url: `${baseURL}/danh-sach/`,
             lastModified: new Date(),
-            changeFrequency: 'weekly',
-            priority: 0.5,
-            // images: ['https://example.com/image.jpg'],
         },
         {
             url: `${baseURL}/tim-kiem/`,
             lastModified: new Date(),
-            changeFrequency: 'weekly',
-            priority: 0.5,
-            // images: ['https://example.com/image.jpg'],
         },
     ];
 }
