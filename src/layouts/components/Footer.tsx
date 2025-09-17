403b96a0014651de771e68407c8208aaccf61d08
nguyentrongbut <cloly031128@gmail.com>
2025-09-17 22:02:21 +0700

    feat(ui): add list tags in footer

--- src/layouts/components/Footer.tsx
+++ src/layouts/components/Footer.tsx
@@ -1,53 +1,83 @@
+// ** Next
 import Link from 'next/link';
 import Image from 'next/image';
+
+type TTags = {
+    title: string;
+    href: string;
+};
+const tags: TTags[] = [
+    {
+        title: 'Truyện tranh',
+        href: '/',
+    },
+    {
+        title: 'Truyện Tranh Online',
+        href: '/',
+    },
+    {
+        title: 'Truyện Tranh Mới',
+        href: '/',
+    },
+    {
+        title: 'Truyện Tranh Hay',
+        href: '/',
+    },
+    {
+        title: 'Đọc Truyện Tranh',
+        href: '/',
+    },
+    {
+        title: 'Manhwa',
+        href: '/the-loai/manhwa.html',
+    },
+    {
+        title: 'Manhua',
+        href: '/the-loai/manhua.html',
+    },
+    {
+        title: 'Manga',
+        href: '/danh-sach/truyen-moi.html',
+    },
+    {
+        title: 'Truyện Ngôn Tình',
+        href: '/the-loai/ngon-tinh.html',
+    },
+];
+
 const Footer = () => {
     return (
         <footer className="w-full">
             <div className="bg-secondary pt-10 pb-5 shadow-custom">
                 <div className="wrapper text-white/70">
-                    <div className="flex justify-between items-end gap-4">
-                        <div className="flex gap-[30px] items-center flex-wrap">
-                            <Link href="/" className="flex items-center">
-                                <Image
-                                    src="/logo.png"
-                                    width={50}
-                                    height={50}
-                                    alt="ztruyện"
-                                ></Image>
-                                <p className="text-lg md:text-[25px] font-bold first-letter:uppercase first-letter:text-primaryColor first-letter:text-2xl md:first-letter:text-4xl">
-                                    ztruyện
-                                </p>
-                            </Link>
-                            <div className="flex flex-wrap text-xs md:text-sm gap-[30px]">
-                                <Link href="/" className="">
-                                    Giới thiệu
-                                </Link>
-                                <Link href="/" className="">
-                                    Liên hệ
-                                </Link>
-                                <Link href="/" className="">
-                                    Điều khoản
-                                </Link>
-                                <Link href="/" className="">
-                                    Chính sách bảo mật
+                    <div className="flex justify-between items-center gap-4">
+                        <div className="grid grid-cols-1 md:grid-cols-2 gap-y-5">
+                            <div>
+                                <Link
+                                    href="/"
+                                    className="inline-flex items-center"
+                                >
+                                    <Image
+                                        src="/logo.png"
+                                        width={50}
+                                        height={50}
+                                        alt="ztruyện"
+                                    ></Image>
+                                    <p className="text-lg md:text-[25px] font-bold first-letter:uppercase first-letter:text-primaryColor first-letter:text-2xl md:first-letter:text-4xl">
+                                        ztruyện
+                                    </p>
                                 </Link>
                             </div>
-                        </div>
-                        <div className="w-[1px] bg-white/70"></div>
-                        <div className="text-xs flex flex-col gap-1 flex-shrink-0">
-                            <h3 className="font-semibold text-sm md:text-base">
-                                Liên hệ đặt quảng cáo
-                            </h3>
-                            <p className="mt-0.5">
-                                Email :
-                                <Link
-                                    href="mailto:ree6i6x@gmail.com"
-                                    className="ml-1"
-                                >
-                                    <i>ree6i6x@gmail.com</i>
-                                </Link>
-                            </p>
-                            <p>Vui lòng gửi email cho chúng tôi</p>
+                            <ul className="flex flex-wrap gap-2">
+                                {tags.map((tag) => (
+                                    <li
+                                        className="py-0.5 px-1 bg-gray-700 text-white rounded-sm text-xs opacity-80"
+                                        key={tag.href}
+                                    >
+                                        <Link href={tag.href}>{tag.title}</Link>
+                                    </li>
+                                ))}
+                            </ul>
                         </div>
                     </div>
                     <div className="mt-6 flex flex-col gap-1.5 text-xs">
