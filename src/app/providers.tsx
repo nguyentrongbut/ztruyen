48f03bc0b687e78f5a1d384b27bc88052d357681
nguyentrongbut <cloly031128@gmail.com>
2025-09-22 10:57:14 +0700

    chore: add event

--- src/app/providers.tsx
+++ src/app/providers.tsx
@@ -11,6 +11,9 @@ export function PostHogProvider({ children }: { children: React.ReactNode }) {
         posthog.init(process.env.NEXT_PUBLIC_POSTHOG_KEY as string, {
             api_host: process.env.NEXT_PUBLIC_POSTHOG_HOST || 'https://us.i.posthog.com',
             person_profiles: 'identified_only', // or 'always' to create profiles for anonymous users as well
+            capture_pageview: true,  // auto track pageview
+            capture_pageleave: true, // track when user leave page
+            autocapture: true,       // click, submit, input, ...
             defaults: '2025-05-24'
         })
     }, [])
